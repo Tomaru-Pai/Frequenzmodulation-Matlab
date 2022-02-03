45aa8a1fbea914ead2fb508b32ceb04d64d65e21
Tomaru-Pai <54865241+Tomaru-Pai@users.noreply.github.com>
2022-02-03 13:35:38 +0100

    Ausarbeitung des Arbeitsauftrages KSN
    Überarbeitung von: 
    >>frequenzmodulation.m
    Hinzufügen von:
    >>arbeitsauftrag lösungen.png

--- frequenzmodulation.m
+++ frequenzmodulation.m
@@ -1,3 +1,5 @@
+% Arbeitsauftrag Thomas Pail %
+
 % Amplitude Modulation (AM)
 
 fs = 500e3; % sample rate 500 kHz
@@ -6,17 +8,17 @@ t = 0:Ts:1-Ts; % time vector start / step / end
 
 % generate waves:
 % wave 1 -> "c" for carrier / Traeger
-fc = 20000; % Hz
+fc = 10000; % Hz
 uc = cos(2 * pi * fc * t);
 
 % wave 2 -> "b" for baseband / Audiosignal
-fb = 2000; % Hz
+fb = 1000; % Hz
 ub = cos(2 * pi * fb * t);
 
 %%%%%%% Modulation %%%%%%%
 % Integral von ub
 % delta_f = 4800; % Frequnezhub  => Zwischen 16kHz und 24kHz kann variert werden % Bei 4800HZ verschwindet der Träger komplet(In AM ganz schlecht)
-eta = 3; % Modulation Index | wenn: eta < 1 => "Narrowband FM", "Schmalband FM"
+eta = 2.4; % Modulation Index | wenn: eta < 1 => "Narrowband FM", "Schmalband FM"
 % Mit Hilfe Bessel Funktion wissen wir => bei 2.4, 5.5 etc. Trägernullstelle
 delta_f = eta * fb;
 int_ub = 1/fs * cumsum(ub);
@@ -26,7 +28,7 @@ ufm = cos(2 * pi * fc * t + 2 * pi * delta_f * int_ub);
 figure(1);
 subplot(2, 1, 1); % 2 rows, 1 column, 1st row
 plot(t, ub, t, ufm), grid on; % ub(t)
-axis([0.0 0.001 -2.2 2.2]);
+axis([0.0 0.003 -2 2]);
 title('time domain');
 xlabel('time');
 ylabel('voltage');
@@ -43,7 +45,7 @@ volt = 2/n * abs(F); % absolute value of amplitude
 subplot(2, 1, 2); % 2nd row
 plot(f, volt); % volt(f)
 grid on;
-axis([0 100e3 0 1.2]);
+axis([0 20e3 0 0.8]);
 title('frequency domain');
 xlabel('frequency');
 ylabel('voltage');
